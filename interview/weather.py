63014b770aea9e663917f426574210f02f6d851e
Tri Lam <tlam@twilio.com>
2022-11-28 01:30:38 -0800

    Solution

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,56 @@
+import csv
+from dataclasses import dataclass
+import collections
+import math
+
+"""
+The idea is to grouping data stream into buckets of (station_name, date)
+and process the data stream as they come in and write
+"""
+
+class TemperatureProcessor:
+    start: str = None
+    end: str = None
+    high:str = -math.inf
+    low:str = math.inf
+    output_fields = ['Station Name','Date','Min Temp','Max Temp','First Temp','Last Temp']
+
+    # data stream is reverse chronological order, 
+    # ie. first temp is the last data, while last temp is first data
+    def process(self, temp) -> None:
+        self.start = temp
+        if self.end is None: self.end = temp
+        self.high = max(self.high, temp)
+        self.low = min(self.low, temp)
+
+class Solution:
+    def __init__(self) -> None:
+        self.memo = collections.defaultdict(TemperatureProcessor)
+
+    def process_stream(self, reader):
+        csv_reader = csv.DictReader(reader)
+        for d in csv_reader:
+            station_name = d['Station Name']
+            date = d['Measurement Timestamp'].split(' ')[0]
+            temp = float(d['Air Temperature'])
+            hash_key = (station_name, date)
+            self.memo[hash_key].process(temp)
+
+    def write_to_csv(self, writer):
+        fields = ['Station Name','Date','Min Temp','Max Temp','First Temp','Last Temp']
+        csv_writer = csv.DictWriter(writer, fieldnames=TemperatureProcessor.output_fields)
+        csv_writer.writeheader()
+        for (name, date), v in self.memo.items():
+            csv_writer.writerow({
+                'Station Name': name,
+                'Date': date,
+                'Min Temp': v.low,
+                'Max Temp': v.high,
+                'First Temp': v.start,
+                'Last Temp': v.end
+            })
 
 def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+    s = Solution()
+    s.process_stream(reader=reader)
+    s.write_to_csv(writer=writer)
--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,47 @@
 from interview import weather
 import io
+import math
+import csv
 
+def test_data_processor_default():
+    p = weather.TemperatureProcessor()
+    assert p.start is None, 'date should default to None if no data been consumed'
+    assert p.end is None, 'date should default to None if no data been consumed'
+    assert math.isinf(p.high) and 1 > p.high, 'high should default to dummy negative infinity if no data been consumed'
+    assert math.isinf(p.low) and 1 < p.low, 'high should default to dummy infinity if no data been consumed'
 
-def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+def test_data_processor():
+    p = weather.TemperatureProcessor()
+    temp = 0
+    p.process(temp)
+    assert p.start == temp, 'should update start from default'
+    assert p.end == temp, 'should update end from default'
+    assert p.high == temp, 'should update high from default'
+    assert p.low == temp, 'should update low from default'
+
+    new_high, new_low = 100, -100
+    p.process(new_high)
+    p.process(new_low)
+    assert p.high == new_high, 'should update high'
+    assert p.low == new_low, 'should update low'
+    assert p.start == new_low, 'should update start every time'
+    assert p.end == temp, 'should NOT update end'
+
+def test_solution():
+    fields = 'Station Name,Measurement Timestamp,Air Temperature'
+    data1 = 'YellowStone,01/01/2022,3'
+    data2 = 'YellowStone,01/01/2022,100'
+    data3 = 'YellowStone,01/01/2022,50'
+    data4 = 'Yosemite,02/02/2022,100'
+    payload = '\n'.join([fields, data1, data2,data3, data4])
+    reader = io.StringIO(payload)
     writer = io.StringIO()
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    
+    rows = writer.getvalue().split('\r\n')
+    rows.remove("")
+    assert len(rows) == 3
+    assert rows[0] == ','.join(weather.TemperatureProcessor.output_fields)
+    assert rows[1] == 'YellowStone,01/01/2022,3.0,100.0,50.0,3.0'
+    assert rows[2] == 'Yosemite,02/02/2022,100.0,100.0,100.0,100.0'
+
